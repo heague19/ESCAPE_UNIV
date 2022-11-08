7518b15508b1738a6f1d391a3f89761c2141d87e
heague19 <heague19@sju.ac.kr>
2022-11-08 15:23:49 +0900

    const int heigth, width; 오류가 나서 삭제 하였습니다. - 이해규-

--- ESCAPE_UNIV/Item.h
+++ ESCAPE_UNIV/Item.h
@@ -1,13 +1,11 @@
 #pragma once
-#include "Cursor_Function.h"
-#define WIDHT 25
+#define WIDTH 25
 #define HEIGTH 40
 class Item
 {
-
 public:
-	int width = 25, height = 40;
-	int Item_Board[HEIGTH + 2][WIDHT + 2];//? 무슨 오류일 까요
+
+	int Item_Board[HEIGTH + 2][WIDTH + 2];//? 무슨 오류일 까요
 	void Constructor_Item_Boundary();
 	void Constructor_Item_Boundary(int Item_board[]);
 };
